386ac1269eaaffcdc90f2d11be9c5845c30ea1c0
Hans <eodnjs5498@naver.com>
2025-07-10 01:34:58 +0900

    module-function name conflict

--- test/test_visualization.py
+++ test/test_visualization.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from visualize_reduced_logits import visualize_reduced_logits
+from visualization import visualize_reduced_logits
 
 import pandas as pd
 import cluster.Cluster as Cluster
