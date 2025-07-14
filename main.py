7cf53652fcd5db8fdb18df3be6297e97e7ca4cbd
Hans <eodnjs5498@naver.com>
2025-07-14 20:01:40 +0900

    Added topic_classifier

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from Classifier import *
-from Clustering import *
-from DataLoader import *
+from Classifier.SentimentClassifier import *
+from Classifier.TopicClassifier import *
+from Clustering.Cluster import *
+from DataLoader.DataLoad import *
 
+import pandas as pd
+import numpy as np
 import argparse
 import tomli
-
 
 def load_config(path: str) -> dict:
     with open(f"./configs/{path}", "rb") as f:
@@ -19,14 +21,32 @@ def parse_args_with_config():
 
     full_parser = argparse.ArgumentParser(description="Full parser with TOML default")
     full_parser = argparse.ArgumentParser(description="Full parser with TOML default")
+
     full_parser.add_argument('--method', type=str, default=config["cluster"]["method"])
     full_parser.add_argument('--column', type=str, default=config["cluster"]["column"])
+    full_parser.add_argument('--topic_filter', type=str, default=config["topic"]["filter"])
+    full_parser.add_argument('--sentiment_filter', type=str, default=config["sentiment"]["filter"])
     full_parser.add_argument('--config', type=str, default=args.config)
 
     return full_parser.parse_args(remaining_args)
 
 if __name__ == "__main__":
     args = parse_args_with_config()
-    print(f"Using config: {args.config}")
-    print(f"Method: {args.method}")
-    print(f"Column: {args.column}")
+
+    ### Print arguments using for loop.
+    print("\nArguments Received [✔]")
+    for key, value in vars(args).items():
+        print(f"    {key:<16}: {value}")
+
+    ### Load Data with preprocessing
+    print("\nExample data >\n")
+    df = load_data('Sentiment_Monitoring_2020-01-01.csv', CSV)
+    df = preprocess_video_title(df)
+
+    # print(df.head(4))
+
+    ### Step 1 : Classify Topic
+    topic_classifier = TopicClassifier()
+    df = topic_classifier.classify(df)
+
+    print(df.head(4))
